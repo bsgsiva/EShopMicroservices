1dfcaa95180c63e8b472daefa530972ab076639f
2000140977 <2000140977@EvolveATM-0>
2025-02-12 16:22:11 +0530

    Changes done

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,38 @@
+﻿ 
+
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :ICommand <CreateProductResult>;
+
+        public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler (IDocumentSession session):
+        ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public  async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product entity
+            //save to db
+            //Return CreateProduct Result result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+               
+
+            };
+
+
+            session.Store(product);
+            session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(Guid.NewGuid());
+             
+        }
+    }
+}
